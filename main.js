784ccea3e096e3395cde781e74ab6ae44d66e8bd
Wataru Sendo <sendo_wataru@cyberagent.co.jp>
2019-04-24 00:48:00 +0900

    hello world!

---
+++ main.js
@@ -0,0 +1,13 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 400, height: 300});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
